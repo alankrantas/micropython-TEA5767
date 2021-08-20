89d7419b6faf81c9ba9db9c6ca94a7711b9bbbdd
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2021-08-21 02:15:42 +0800

    Update TEA5767.py

--- TEA5767.py
+++ TEA5767.py
@@ -1,24 +1,20 @@
 """
-MicroPython ESP8266/ESP32 driver for TEA5767 FM radio module:
+MicroPython driver for TEA5767 FM radio module:
 https://github.com/alankrantas/micropython-TEA5767
 
 TEA5767 Datasheet:
 https://www.sparkfun.com/datasheets/Wireless/General/TEA5767.pdf
 
 MIT License
-
-Copyright (c) 2020 Alan Wang
-
+Copyright (c) 2021 Alan Wang
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,17 +24,25 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+import time
+
 class Radio:
     """
    TEA5767 FM radio driver class.
     
     Initialize:
-        radio = TEA5767.Radio(i2c, [addr=0x60, freq=99.7, band="US", stereo=True,
+        radio = TEA5767.Radio(i2c, [addr=0x60, freq=99.7, band='US', stereo=True,
                                     soft_mute=True, noise_cancel=True, high_cut=True])
     """
     
     FREQ_RANGE_US = (87.5, 108.0)
     FREQ_RANGE_JP = (76.0, 91.0)
+    ADC = (0, 5, 7, 10)
+    ADC_BIT = (0, 1, 2, 3)
+    
+    __slot__ = ['_i2c', '_address', 'frequency', 'band_limits', 'standby_mode', 'mute_mode', 'soft_mute_mode',
+                'search_mode', 'search_direction', 'search_adc_level', 'stereo_mode', 'stereo_noise_cancelling_mode',
+                'high_cut_mode', 'is_ready', 'is_stereo', 'is_stereo', 'signal_adc_level']
     
     def __init__(self, i2c, addr=0x60, freq=0.0, band='US', stereo=True,
                             soft_mute=True, noise_cancel=True, high_cut=True):
@@ -72,67 +76,55 @@ class Radio:
     def search(self, mode, dir=1, adc=7):
         self.search_mode = mode
         self.search_direction = dir
-        self.search_adc_level = adc if adc in (10, 7, 5, 0) else 7
+        self.search_adc_level = adc if adc in Radio.ADC else 7
         self.update()
 
     def mute(self, mode):
         self.mute_mode = mode
         self.update()
-        
+    
     def standby(self, mode):
         self.standby_mode = mode
         self.update()
 
     def read(self):
         buf = self._i2c.readfrom(self._address, 5)
-        freqB = int((buf[0] & 0x3f) * (1 << 8) + buf[1])
+        freqB = int((buf[0] & 0x3f) << 8 | buf[1])
         self.frequency = round((freqB * 32768 / 4 - 225000) / 1000000, 1)
         self.is_ready = int(buf[0] >> 7) == 1
         self.is_stereo = int(buf[2] >> 7) == 1
         self.signal_adc_level = int(buf[3] >> 4)
 
     def update(self):
-        buf = bytearray(5)
-        if self.band_limits == 'US':
-            if self.frequency < self.FREQ_RANGE_US[0]:
-                self.frequency = self.FREQ_RANGE_US[0]
-            elif self.frequency > self.FREQ_RANGE_US[1]:
-                self.frequency = self.FREQ_RANGE_US[1]
-        else:
-            self.band_limits = 'JP'
-            if self.frequency < self.FREQ_RANGE_JP[0]:
-                self.frequency = self.FREQ_RANGE_JP[0]
-            elif self.frequency > self.FREQ_RANGE_JP[1]:
-                self.frequency = self.FREQ_RANGE_JP[1]
-        freqB = 4 * (self.frequency * 1000000 + 225000) / 32768
-        buf[0] = int(freqB) >> 8
-        if self.mute_mode:
-            buf[0] += 1 << 7
-        if self.search_mode:
-            buf[0] += 1 << 6
-        buf[1] = int(freqB) & 0xff
-        buf[2] = 1 << 4
-        if self.search_direction == 1:
-            buf[2] += 1 << 7
-        if self.search_adc_level == 10:
-            buf[2] += 3 << 5
-        elif self.search_adc_level == 7:
-            buf[2] += 2 << 5
-        elif self.search_adc_level == 5:
-            buf[2] += 1 << 5
-        if self.stereo_mode:
-            buf[2] += 1 << 3
-        buf[3] = 1 << 4
-        if self.standby_mode:
-            buf[3] += 1 << 6
+        
         if self.band_limits == 'JP':
-            buf[3] += 1 << 5
-        if self.soft_mute_mode:
-            buf[3] += 1 << 3
-        if self.high_cut_mode:
-            buf[3] += 1 << 2
-        if self.stereo_noise_cancelling_mode:
-            buf[3] += 1 << 1
+            self.frequency = min(max(self.frequency, Radio.FREQ_RANGE_JP[0]), Radio.FREQ_RANGE_JP[1])
+        else:
+            self.band_limits = 'US'
+            self.frequency = min(max(self.frequency, Radio.FREQ_RANGE_US[0]), Radio.FREQ_RANGE_US[1])
+        freqB = 4 * (self.frequency * 1000000 + 225000) / 32768
+        buf = bytearray(5)
+        buf[0] = int(freqB) >> 8 | self.mute_mode << 7 | self.search_mode << 6
+        buf[1] = int(freqB) & 0xff
+        buf[2] = self.search_direction << 7 | 1 << 4 | self.stereo_mode << 3
+        try:
+            buf[2] += Radio.ADC_BIT[Radio.ADC.index(self.search_adc_level)] << 5
+        except:
+            pass
+        buf[3] = self.standby_mode << 6 | (self.band_limits == 'JP') << 5 | 1 << 4
+        buf[3] += self.soft_mute_mode << 3 | self.high_cut_mode << 2 | self.stereo_noise_cancelling_mode << 1
         buf[4] = 0
         self._i2c.writeto(self._address, buf)
+        time.sleep_ms(50)
         self.read()
+
+
+if __name__ == '__main__':
+    
+    from machine import Pin, SoftI2C
+    i2c = SoftI2C(scl=Pin(27), sda=Pin(26), freq=400000)
+    
+    radio = Radio(i2c, freq=99.7)
+    print('Frequency: FM {}\nReady: {}\nStereo: {}\nADC level: {}'.format(
+        radio.frequency, radio.is_ready,  radio.is_stereo, radio.signal_adc_level
+        ))
