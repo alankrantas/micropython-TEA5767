132e85d64756ca6734d90f8666346b163397a3fb
Alan Wang <krantas@gmail.com>
2023-07-07 09:58:44 +0800

    Update TEA5767.py

--- TEA5767.py
+++ TEA5767.py
@@ -31,7 +31,7 @@ class Radio:
    TEA5767 FM radio driver class.
     
     Initialize:
-        radio = TEA5767.Radio(i2c, [addr=0x60, freq=99.7, band='US', stereo=True,
+        radio = TEA5767.Radio(i2c, [addr=0x60, freq=106.7, band='US', stereo=True,
                                     soft_mute=True, noise_cancel=True, high_cut=True])
     """
     
@@ -42,7 +42,7 @@ class Radio:
     
     __slot__ = ['_i2c', '_address', 'frequency', 'band_limits', 'standby_mode', 'mute_mode', 'soft_mute_mode',
                 'search_mode', 'search_direction', 'search_adc_level', 'stereo_mode', 'stereo_noise_cancelling_mode',
-                'high_cut_mode', 'is_ready', 'is_stereo', 'is_stereo', 'signal_adc_level']
+                'high_cut_mode', 'is_ready', 'is_stereo', 'signal_adc_level']
     
     def __init__(self, i2c, addr=0x60, freq=0.0, band='US', stereo=True,
                             soft_mute=True, noise_cancel=True, high_cut=True):
@@ -114,7 +114,7 @@ class Radio:
         buf[3] += self.soft_mute_mode << 3 | self.high_cut_mode << 2 | self.stereo_noise_cancelling_mode << 1
         buf[4] = 0
         self._i2c.writeto(self._address, buf)
-        time.sleep_ms(50)
+        time.sleep_ms(1)  # i2c bus has max delay of 400 us
         self.read()
 
 
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     from machine import Pin, SoftI2C
     i2c = SoftI2C(scl=Pin(5), sda=Pin(4), freq=400000)
     
-    radio = Radio(i2c, freq=99.7)
+    radio = Radio(i2c, freq=106.7)
     print('Frequency: FM {}\nReady: {}\nStereo: {}\nADC level: {}'.format(
         radio.frequency, radio.is_ready,  radio.is_stereo, radio.signal_adc_level
         ))
